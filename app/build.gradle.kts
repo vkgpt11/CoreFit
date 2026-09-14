b8c352eaad9966bd955665c9c82624ad1caecd3b
Vikas Kumar Gupta <vikasgupta.net@live.com>
2026-09-14 22:34:41 +0530

    Use Android API 36 for CI build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.simpleexercisecounter"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.simpleexercisecounter"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
